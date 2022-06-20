90cb8b557e0cd155aa89b4c55eadcf0ab25dcb26
Nelito Junior <ngvjqp@gmail.com>
2022-06-20 16:08:40 -0300

    Translate as env variable

--- video_creation/voices.py
+++ video_creation/voices.py
@@ -4,13 +4,21 @@ from mutagen.mp3 import MP3
 from utils.console import print_step, print_substep
 from rich.progress import track
 from googletrans import Translator
+from dotenv import load_dotenv
+
+import os
+
 
 def save_text_to_mp3(reddit_obj):
     """Saves Text to MP3 files.
- 
+
     Args:
         reddit_obj : The reddit object you received from the reddit API in the askreddit.py file.
     """
+    load_dotenv()
+
+    translate = os.getenv("TRANSLATE", default="no").casefold()
+
     print_step("Saving Text to MP3 files...")
     length = 0
 
@@ -21,10 +29,16 @@ def save_text_to_mp3(reddit_obj):
     thread_title = reddit_obj["thread_title"]
     # translate the thread_title to Portuguese with googletrans
     translator = Translator()
-    thread_title_pt = translator.translate(thread_title, src='en', dest='pt')
-    print(thread_title_pt.text)
+    if translate == "yes":
+        thread_title_pt = translator.translate(
+            thread_title, src='en', dest='pt').text
+    else:
+        thread_title_pt = thread_title
 
-    tts = gTTS(text=thread_title_pt.text, lang="pt-br", slow=False, tld="com.br")
+    print(thread_title_pt)
+
+    tts = gTTS(text=thread_title_pt,
+               lang="pt-br", slow=False, tld="com.br")
     tts.save(f"assets/mp3/title.mp3")
     length += MP3(f"assets/mp3/title.mp3").info.length
 
@@ -34,20 +48,31 @@ def save_text_to_mp3(reddit_obj):
         pass
 
     if reddit_obj["thread_post"] != "":
-        thread_post_pt = translator.translate(reddit_obj["thread_post"], src='en', dest='pt')
-        tts = gTTS(text=thread_post_pt.text, lang="pt-BR", slow=False, tld="com.br")
+        if translate == "yes":
+            thread_post_pt = translator.translate(
+                reddit_obj["thread_post"], src='en', dest='pt').text
+        else:
+            thread_post_pt = reddit_obj["thread_post"]
+
+        tts = gTTS(text=thread_post_pt,
+                   lang="pt-BR", slow=False, tld="com.br")
         tts.save(f"assets/mp3/posttext.mp3")
         length += MP3(f"assets/mp3/posttext.mp3").info.length
-
 
     for idx, comment in track(enumerate(reddit_obj["comments"]), "Saving..."):
         # ! Stop creating mp3 files if the length is greater than 90 seconds and there is at least one comment.
         if length > 90 and idx > 0:
             break
         print(comment["comment_body"])
-        comment_body_pt = translator.translate(comment["comment_body"], src='en', dest='pt')
-        print(comment_body_pt.text)
-        tts = gTTS(text=comment_body_pt.text, lang="pt-BR", slow=False, tld="com.br")
+        if translate == "yes":
+            comment_body_pt = translator.translate(
+                comment["comment_body"], src='en', dest='pt').text
+        else:
+            comment_body_pt = comment["comment_body"]
+
+        print(comment_body_pt)
+        tts = gTTS(text=comment_body_pt,
+                   lang="pt-BR", slow=False, tld="com.br")
         tts.save(f"assets/mp3/{idx}.mp3")
         length += MP3(f"assets/mp3/{idx}.mp3").info.length
 
